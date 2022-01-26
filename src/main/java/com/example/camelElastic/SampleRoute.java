389c7e1b643835bd9b2cc44b183c24cb398eac54
saavedrah <harold.saavedra@monoplus.com>
2022-01-26 11:10:48 +0900

    Added timer - Class not found in ElasticSearch

--- src/main/java/com/example/camelElastic/SampleRoute.java
+++ src/main/java/com/example/camelElastic/SampleRoute.java
@@ -1,12 +1,20 @@
 package com.example.camelElastic;
 
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SampleRoute  extends RouteBuilder {
     @Override
     public void configure() throws Exception {
+
+        CamelContext camelContext = new DefaultCamelContext();
+
+        from("timer://foo?fixedRate=true&period=60000")
+                .to("direct:search");
+
         from("direct:search")
                 .to("elasticsearch-rest://elasticsearch?operation=Search&indexName=twitter");
     }
